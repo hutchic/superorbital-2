4ba31d347518f611a42d744793b08a5898fd7a01
Colin Hutchinson <chutchic@gmail.com>
2023-03-06 18:21:36 +0000

    feat(list): first pass at drone list

--- exercises/languages/python/drone_cli.py
+++ exercises/languages/python/drone_cli.py
@@ -1,11 +1,20 @@
 # -*- coding: utf-8 -*-
 import fire
+import swagger_client
+from swagger_client.rest import ApiException
+from pprint import pprint
 
 
 class DroneCLI:
     """
     a cli for interacting with the drones api
     """
+
+    def __init__(self):
+        configuration = swagger_client.Configuration()
+        configuration.host = "http://localhost:8080"
+        configuration.api_key["Authorization"] = "OMGSOSEKRET"
+        self.api_client = swagger_client.ApiClient(configuration)
 
     def create(self, filename):
         """
@@ -20,7 +29,12 @@ class DroneCLI:
         lists drones
         :returns list of drones
         """
-        pass
+        try:
+            api_instance = swagger_client.DroneApi(self.api_client)
+            api_response = api_instance.list_drones()
+            pprint(api_response)
+        except ApiException as e:
+            print("Exception when calling DroneApi->list_drones: %s\n" % e)
 
 
 if __name__ == "__main__":
